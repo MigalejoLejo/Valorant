d824856f623ad2b536f17815e793fb2d8b901573
Miguel Alejandro Correa (PWC) <miguelalejandro.correa.avila@pwc.com>
2023-05-24 09:32:13 +0100

    Initial Commit

---
+++ Valorant/ValorantApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValorantApp.swift
+//  Valorant
+//
+//  Created by Miguel Alejandro Correa Avila on 24/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ValorantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
